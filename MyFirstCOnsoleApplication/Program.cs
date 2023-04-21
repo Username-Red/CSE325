67fcd78b90331b9b1fcb0a43dfae5aaeb483c366
Username-Red <ashkestrel@gmail.com>
2023-04-21 23:01:24 +0200

    Add project files.

---
+++ MyFirstCOnsoleApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿string name = "Red";
+string location = "South Africa";
+DateTime today = DateTime.UtcNow.Date;
+DateTime christmas = new DateTime(today.Year, 12, 25);
+double daysToChristmas = (christmas - today).TotalDays;
+Console.WriteLine($"My name is {name}, I am from {location}.");
+Console.WriteLine(today.ToString("dd/MM/yyyy"));
+Console.WriteLine($"There are {daysToChristmas}");
+
