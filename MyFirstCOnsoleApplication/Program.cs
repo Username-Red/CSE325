dfb68ba78a792d3370c9be6c7a915ab0d7c7da9f
Username-Red <ashkestrel@gmail.com>
2023-04-22 13:54:45 +0200

    let's give this a try

--- MyFirstCOnsoleApplication/Program.cs
+++ MyFirstCOnsoleApplication/Program.cs
@@ -1,9 +1,37 @@
-﻿string name = "Red";
-string location = "South Africa";
-DateTime today = DateTime.UtcNow.Date;
-DateTime christmas = new DateTime(today.Year, 12, 25);
-double daysToChristmas = (christmas - today).TotalDays;
-Console.WriteLine($"My name is {name}, I am from {location}.");
-Console.WriteLine(today.ToString("dd/MM/yyyy"));
-Console.WriteLine($"There are {daysToChristmas}");
+﻿using System;
+class GlazerCalc
+{
+    static void Main()
+    {
+        string name = "Red";
+        string location = "South Africa";
+        DateTime today = DateTime.UtcNow.Date;
+        DateTime christmas = new DateTime(today.Year, 12, 25);
+        double daysToChristmas = (christmas - today).TotalDays;
+        Console.WriteLine($"My name is {name}, I am from {location}.");
+        Console.WriteLine(today.ToString("dd/MM/yyyy"));
+        Console.WriteLine($"There are {daysToChristmas} days until Christmas!");
+
+
+
+        // Copied from the yellow book, not my code
+        double width, height, woodLength, glassArea;
+        string widthString, heightString;
+        widthString = Console.ReadLine();
+        width = double.Parse(widthString);
+        heightString = Console.ReadLine();
+        height = double.Parse(heightString);
+        woodLength = 2 * (width + height) * 3.25;
+        glassArea = 2 * (width * height);
+        Console.WriteLine("The length of the wood is " +
+        woodLength + " feet");
+        Console.WriteLine("The area of the glass is " +
+        glassArea + " square metres");
+
+        Console.ReadKey(true);
+    }
+}
+
+
+
 
