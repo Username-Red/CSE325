fdaf86e06ab40988c730d584b4bf5730f53a92b1
emoryhubbard <edhthe4th@gmail.com>
2023-05-12 17:06:18 -0400

    Testing github with comment

--- MegaDesk - Greeff/MegaDesk - Greeff/Program.cs
+++ MegaDesk - Greeff/MegaDesk - Greeff/Program.cs
@@ -3,7 +3,7 @@ namespace MegaDesk___Greeff
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        ///  The main entry point for the application. The main function. -- Emory
         /// </summary>
         [STAThread]
         static void Main()
